3704fdb213896c6b730a4fdc18b1bb928e3c064f
Ivan K. <i.m.krot.frontend@gmail.com>
2024-06-30 14:12:24 +0300

    Content management (updating list of lots )

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -30,99 +31,10 @@ import { RouterModule } from '@angular/router';
   <section class="content">
     <router-outlet></router-outlet>
   </section>
-  <footer>
-    <div class="footer-content">
-      <div class="footer-section">
-        <h3 class="footer-heading">More information</h3>
-        <div class="footer-content">
-          <ul class="footer-list">
-            <li>
-              <a href="">Link 1</a>
-            </li>
-            <li>
-              <a href="">Link 2</a>
-            </li>
-            <li>
-              <a href="">Link 3</a>
-            </li>
-            <li>
-              <a href="">Link 4</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div class="footer-section">
-        <h3 class="footer-heading">Terms of use</h3>
-        <div class="footer-content">
-          <ul class="footer-list">
-            <li>
-              <a href="">Terms of use 1</a>
-            </li>
-            <li>
-              <a href="">Terms of use 2</a>
-            </li>
-            <li>
-              <a href="">Terms of use 3</a>
-            </li>
-            <li>
-              <a href="">Terms of use 4</a>
-            </li>
-            <li>
-              <a href="">Terms of use 5</a>
-            </li>
-            <li>
-              <a href="">Terms of use 6</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div class="footer-section">
-        <h3 class="footer-heading">Useful information</h3>
-        <div class="footer-content">
-          <ul class="footer-list">
-            <li>
-              <a href="">Collaboration</a>
-            </li>
-            <li>
-              <a href="">Partners</a>
-            </li>
-            <li>
-              <a href="">FAQ</a>
-            </li>
-          </ul>
-        </div>
-        
-      </div>
-      <div class="footer-section">
-        <h3 class="footer-heading">Platform information</h3>
-        <div class="footer-content">
-          <ul class="footer-list">
-            <li>
-              <a href="">Team</a>
-            </li>
-            <li>
-              <a href="">GitHub</a>
-            </li>
-            <li>
-              <a href="">Linkedin</a>
-            </li>
-            <li>
-              <a href="">Report an issue</a>
-            </li>
-            <li>
-              <a href="">I have an idea!</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-    <div class="copyright">
-        <a href="">All rights reserved. Extablished in 2024</a>
-      </div>
-  </footer>
+  <app-footer></app-footer>
   `,
   styleUrls: ['./app.component.css'],
-  imports: [HomeComponent, RouterModule],
+  imports: [HomeComponent, FooterComponent, RouterModule],
 })
 export class AppComponent {
   title = 'Chariction';
