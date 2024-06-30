ff10391785493b4a097961c89b21cf7acaef6704
Ivan K. <i.m.krot.frontend@gmail.com>
2024-06-30 09:00:35 +0300

    Footer implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,15 +13,113 @@ import { RouterModule } from '@angular/router';
           <img class="brand-logo" src="/assets/charicticon_logo/logo_main_compressed.png" alt="logo" aria-hidden="true">
         </a>
       </div>
-      <div class="donate"><a href="https://send.monobank.ua/jar/5GDiCGtwBs" class="jar">Quick Donate</a></div>
-      <div>
-        <a class="contact-us" href="#">Contact Us</a>
+      <div class="navigation-links">
+        <div class="donate"><a href="https://send.monobank.ua/jar/5GDiCGtwBs" class="jar">Quick Donate</a></div>
+        <div class="contact-us">
+          <a href="#">Contact Us</a>
+        </div>
+        <div class="my-account">
+          <a href="#">My Account</a>
+        </div>
+        <div class="language-component">
+          <a class="language-dropdown" href="#">Language: EN (tbd)</a>
+        </div>
       </div>
     </header>
   </main>
   <section class="content">
     <router-outlet></router-outlet>
   </section>
+  <footer>
+    <div class="footer-content">
+      <div class="footer-section">
+        <h3 class="footer-heading">More information</h3>
+        <div class="footer-content">
+          <ul class="footer-list">
+            <li>
+              <a href="">Link 1</a>
+            </li>
+            <li>
+              <a href="">Link 2</a>
+            </li>
+            <li>
+              <a href="">Link 3</a>
+            </li>
+            <li>
+              <a href="">Link 4</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="footer-section">
+        <h3 class="footer-heading">Terms of use</h3>
+        <div class="footer-content">
+          <ul class="footer-list">
+            <li>
+              <a href="">Terms of use 1</a>
+            </li>
+            <li>
+              <a href="">Terms of use 2</a>
+            </li>
+            <li>
+              <a href="">Terms of use 3</a>
+            </li>
+            <li>
+              <a href="">Terms of use 4</a>
+            </li>
+            <li>
+              <a href="">Terms of use 5</a>
+            </li>
+            <li>
+              <a href="">Terms of use 6</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="footer-section">
+        <h3 class="footer-heading">Useful information</h3>
+        <div class="footer-content">
+          <ul class="footer-list">
+            <li>
+              <a href="">Collaboration</a>
+            </li>
+            <li>
+              <a href="">Partners</a>
+            </li>
+            <li>
+              <a href="">FAQ</a>
+            </li>
+          </ul>
+        </div>
+        
+      </div>
+      <div class="footer-section">
+        <h3 class="footer-heading">Platform information</h3>
+        <div class="footer-content">
+          <ul class="footer-list">
+            <li>
+              <a href="">Team</a>
+            </li>
+            <li>
+              <a href="">GitHub</a>
+            </li>
+            <li>
+              <a href="">Linkedin</a>
+            </li>
+            <li>
+              <a href="">Report an issue</a>
+            </li>
+            <li>
+              <a href="">I have an idea!</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="copyright">
+        <a href="">All rights reserved. Extablished in 2024</a>
+      </div>
+  </footer>
   `,
   styleUrls: ['./app.component.css'],
   imports: [HomeComponent, RouterModule],
